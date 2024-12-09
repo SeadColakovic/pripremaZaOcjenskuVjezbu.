023c87bc50301f0895435975f66e84c65d4bdcd2
SeadColakovic <flegarsead263@gmail.com>
2024-12-09 13:43:39 +0100

    Add files via upload

---
+++ pripremaZaocjenskuvjezba2/pripremaZaocjenskuvjezba2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pripremaZaocjenskuvjezba2
+{
+    internal class Program
+    
+
+    {
+        static void Main()
+        {
+            for (int year = 1900; year <= 2100; year++)
+            {
+                if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
+                {
+                    Console.WriteLine(year);
+                }
+            }
+        }
+    }
+
+}
