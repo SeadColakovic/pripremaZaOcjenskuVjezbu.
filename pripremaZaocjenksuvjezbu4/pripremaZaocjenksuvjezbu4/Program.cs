4c9d2386a5180357ac9ae1aaee3b156dc0124b86
SeadColakovic <flegarsead263@gmail.com>
2024-12-09 13:47:49 +0100

    Add files via upload

---
+++ pripremaZaocjenksuvjezbu4/pripremaZaocjenksuvjezbu4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pripremaZaocjenksuvjezbu4
+{
+  
+    class KlasaX
+    {
+        private int broj = 10;
+
+        public int Broj
+        {
+            get { return broj; }
+            set { broj = value; }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KlasaX instanca = new KlasaX();
+            Console.WriteLine(instanca.Broj);
+        }
+    }
+
+}
